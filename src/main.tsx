9dd243af8f9d78ca353f21820a8693735eb423a2
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-09-22 20:41:35 -0300

    firt commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -5,16 +6,21 @@ import { LoginProvider } from './Common/Context/Login'
 import { SignUpProvider } from './Common/Context/SignUp'
 import { Rotas } from './Route'
 import theme from './Style/Global'
+const client = new ApolloClient({
+  uri: 'https://ooipsum.herokuapp.com/',
+  cache: new InMemoryCache()
+})
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <LoginProvider>
-      <SignUpProvider>
-
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Rotas />
-        </ThemeProvider>
-      </SignUpProvider>
-    </LoginProvider>
+    <ApolloProvider client={client}>
+      <LoginProvider>
+        <SignUpProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Rotas />
+          </ThemeProvider>
+        </SignUpProvider>
+      </LoginProvider>
+    </ApolloProvider>
   </React.StrictMode>
 )
